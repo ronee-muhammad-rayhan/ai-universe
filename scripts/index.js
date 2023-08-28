8f114e71ee15ba5ee883b0c28690ad0083e8568a
ronee-muhammad-rayhan <muhammad@rayhanworld.com>
2023-08-28 15:51:45 +0600

    getting all data array at console

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
-console.log('object');
+function loadData() {
+    const url = 'https://openapi.programming-hero.com/api/ai/tools'
+    // const res = fetch(url)
+    // const data = res.json()
+    // console.log(data);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data.data.tools)
+            const array = data.data.tools;
+            array.forEach(element => {
+                console.log(element);
+            });
+        });
+}
+
+loadData();
